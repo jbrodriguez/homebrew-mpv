b19bbdd93f14842b322e9dc0df0ec010315c3e67
Juan B. Rodriguez <jbrodriguez@gmail.com>
2019-01-27 08:53:34 -0500

    Update revision

--- Formula/mpv-opts.rb
+++ Formula/mpv-opts.rb
@@ -3,7 +3,7 @@ class MpvOpts < Formula
 	homepage "https://mpv.io"
 	url "https://github.com/mpv-player/mpv/archive/v0.29.1.tar.gz"
 	sha256 "f9f9d461d1990f9728660b4ccb0e8cb5dce29ccaa6af567bec481b79291ca623"
-	revision 2
+	revision 3
 	head "https://github.com/mpv-player/mpv.git"
   
 	bottle do
